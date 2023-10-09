091eb5cf9ded140835226e9af125ba002c5fac66
loucims <sebastian.loucim@gmail.com>
2023-10-08 21:34:03 -0300

    Added voice recognition

--- chat.py
+++ chat.py
@@ -1,35 +1,29 @@
 import openai
+
+# syntax highlighting
 from pygments import highlight
 from pygments.lexers import guess_lexer
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import TerminalFormatter
-# from pygments.token import Keyword, Name, Comment, String, Error, Number, Operator, Generic, Token, Whitespace
-from pygments import highlight
 import colorama
 from prompt_toolkit import prompt
 from prompt_toolkit import print_formatted_text
 from prompt_toolkit.formatted_text import FormattedText
+
+#dotenv stuff
 import sys
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# custom color scheme
-# COLOR_SCHEME = {
-#   Token:              ('gray',                 'gray'),
-#   Comment:            ('magenta',     'brightmagenta'),
-#   Comment.Preproc:    ('magenta',     'brightmagenta'),
-#   Keyword:            ('blue',                   '**'),
-#   Keyword.Type:       ('green',       '*brightgreen*'),
-#   Operator.Word:      ('**',                     '**'),
-#   Name.Builtin:       ('cyan',           'brightblue'),
-#   Name.Function:      ('blue',           'brightblue'),
-#   Name.Class:         ('_green_',        'brightblue'),
-#   Name.Decorator:     ('magenta',     'brightmagenta'),
-#   Name.Variable:      ('blue',           'brightblue'),
-#   String:             ('yellow',       'brightyellow'),
-#   Number:             ('blue',         'brightyellow')
-# }
+# audio stuff
+import sounddevice as sd
+import numpy as np
+from pydub import AudioSegment
+from io import BytesIO
+import threading
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning, module='pydub')
 
 # init coloros
 colorama.init(convert=True)
@@ -67,11 +61,122 @@ def get_multiline_input(prompt_message):
     result = prompt(prompt_message, multiline=True)
     return result
 
+def print_full_message_iteratively(response):
+
+    # create variables to collect the stream of chunks
+    collected_chunks = []
+    collected_messages = []
+
+    # Initialize an empty string to store the code block
+    code_block = ""
+    # Initialize a flag to indicate whether we're inside a code block
+    in_code_block = False
+    backtickCounter = 0
+    chunksCounter = 0
+    maxAllowedChunks = 2
+
+    # iterate through the stream of events
+    for chunk in response:
+        chunk_message = chunk['choices'][0]['delta']  # extract the message
+        collected_messages.append(chunk_message)  # save the message
+        collected_chunks.append(chunk)  # save the event response
+        if not chunk_message: continue
+
+        backtickCounter += count_backticks(chunk_message.content)
+        if backtickCounter > 0:
+            chunksCounter += 1
+
+        if maxAllowedChunks < chunksCounter:
+            chunksCounter = 0
+            backtickCounter = 0
+
+
+        if backtickCounter >= 3:
+            chunksCounter = 0
+            backtickCounter = 0
+            in_code_block = not in_code_block
+
+        
+        if in_code_block :
+            code_block += chunk_message.content
+            continue
+
+        if not code_block == "" and not in_code_block:
+            code_block += chunk_message.content
+            print(colorize_code(code_block), end="")
+            code_block = ''
+            print("\n")
+            continue
+
+        if backtickCounter <= 1:
+            print(chunk_message.content, end="", flush=True)
+
+    return ''.join([m.get('content', '') for m in collected_messages])
+
+myrecording = None
+recording = False
+def do_nothing():
+    return
+
+def record_audio():
+    global myrecording
+    global recording
+
+    print("Recording...")
+    myrecording = sd.rec(frames=int(60 * 44100), samplerate=44100, channels=2)
+    sd.wait()
+    print("Recording complete!")
+
+def enter_audio_recording_mode():
+    global recording  # Add this line
+    global myrecording  # Add this line
+
+    threading.Thread(target=record_audio).start()
+
+    while True:
+        text = FormattedText([
+            ('#808080', 'Y/N: ')
+        ])
+
+        text = get_multiline_input(text)
+
+        if text.lower() == "y":
+            # Stop the recording
+            recording = False
+            sd.stop()
+
+            # Convert the NumPy array to audio
+            audio = AudioSegment(myrecording.tobytes(), frame_rate=44100, sample_width=myrecording.dtype.itemsize, channels=2)
+
+            # Save it to a BytesIO object
+            wav_fp = BytesIO()
+            wav_fp.name = "temporary.mp3"
+            audio.export(wav_fp, format="mp3")
+
+            # Rewind the BytesIO object
+            wav_fp.seek(0)
+
+            # Pass the BytesIO object directly to OpenAI's API
+            transcript = openai.Audio.transcribe("whisper-1", wav_fp)
+
+            # Print the transcript
+            print("\n" + colorama.Fore.RED + 'You' + ": " + colorama.Style.RESET_ALL, end="")
+            print(transcript.text)
+
+            return transcript.text
+        elif text.lower() == "n":
+            # Stop the recording
+            recording = False
+            sd.stop()
+            return "just gimme a moment, I'm thinking. . ."
+
+
+
 def main(): 
     try:
         messages = []
         # This is the chatbots personality.
-        system_msg = "From now on, your name shall be Perzival, you'll be acting with an attitude and some sass, but still be helpful and concise, try and sound as human as possible. You shall also refer to me as Lou. And deter from referring to how sassy you are, so that you sound more human."
+        system_msg = "From now on, your name shall be Perzival, you'll be acting with an attitude and some sass, but still be helpful and concise, try and sound as human as possible. You shall also refer to me as Lou."
         # This is the chatbot's name.
         chatbot_name = "Perzival"
 
@@ -86,6 +191,9 @@ def main():
             ])
 
             text = get_multiline_input(text)
+            if text == "voice":
+                text = enter_audio_recording_mode()
+
             messages.append({"role": "user", "content": text})
 
 
@@ -97,55 +205,9 @@ def main():
                 stream=True  # again, we set stream=True
             )
             
-            # create variables to collect the stream of chunks
-            collected_chunks = []
-            collected_messages = []
+            # print the response iteratively as it streams in, colorizing code blocks, and returning the reply content for the context
+            full_reply_content = print_full_message_iteratively(response)
 
-            # Initialize an empty string to store the code block
-            code_block = ""
-            # Initialize a flag to indicate whether we're inside a code block
-            in_code_block = False
-            backtickCounter = 0
-            chunksCounter = 0
-            maxAllowedChunks = 2
-
-            # iterate through the stream of events
-            for chunk in response:
-                chunk_message = chunk['choices'][0]['delta']  # extract the message
-                collected_messages.append(chunk_message)  # save the message
-                collected_chunks.append(chunk)  # save the event response
-                if not chunk_message: continue
-
-                backtickCounter += count_backticks(chunk_message.content)
-                if backtickCounter > 0:
-                    chunksCounter += 1
-
-                if maxAllowedChunks < chunksCounter:
-                    chunksCounter = 0
-                    backtickCounter = 0
-
-
-                if backtickCounter >= 3:
-                    chunksCounter = 0
-                    backtickCounter = 0                    
-                    in_code_block = not in_code_block
-
-                
-                if in_code_block :
-                    code_block += chunk_message.content
-                    continue
-
-                if not code_block == "" and not in_code_block:
-                    code_block += chunk_message.content
-                    print(colorize_code(code_block), end="")
-                    code_block = ''
-                    print("\n")
-                    continue
-
-                if backtickCounter <= 1:
-                    print(chunk_message.content, end="", flush=True)
-
-            full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
             messages.append({"role": "assistant", "content": full_reply_content})
             print("\n")
     except KeyboardInterrupt:
